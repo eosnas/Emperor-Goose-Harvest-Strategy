5b19cc96b62453fe291fd6288883cc78796a0429
Erik Osnas <erik_osnas@fws.gov>
2022-11-21 16:44:01 -0900

    edited population sim function so that 2016 posterior could be run; modified optimization to use 2016 posterior

--- simulate.pop.R
+++ simulate.pop.R
@@ -137,93 +137,93 @@ out <- readRDS("out2.RDS")
 #Now add harvest regulation rule
 # if observed pop < 23000, then harvest = historical harvest
 # if observed pop >= 23000, then harvest as above rnorm(mu.green, sigma.har)
-project.pop <- function(
-  #population projection at constant parameters is default
-  Tmax = 100,
-  n1 = out$mean$N.tot, #need all population size estimates so we can translate to harvest rate
-  r = out$mean$r.max, 
-  theta = out$mean$theta,
-  K = out$mean$CC, 
-  Hgreen = out$mean$mu.green,
-  Hred = out$mean$m.har, 
-  # sdH = NA,
-  sdpop = NA,
-  q = out$mean$q, 
-  t_close = 23000, #closure threshold, 23000 is 2016 management plan default
-  total = TRUE, 
-  stochastic = TRUE){
-  
-  pop <- har <- hunt <- numeric() #rep(0, Tmax)
-  pop[1] <- n1[length(n1)] #starting population size
-  pop[1] <- n1[37] #starting population size
-  crip <- rbeta(1, 50, 150) #mean for rbeta =0.25, beta distribution parameters, could sample from posterior
-  hred <- mean(Hred/n1[1:32])
-  hgreen <- mean(Hgreen/n1[33:37])
-  sdh <- sd(Hred/n1[1:32])
-  s <- rchisq(1, 35) #linear model for observation error
-
-  for(t in 2:(Tmax)){
-    if(stochastic == FALSE){
-      mu <- q*pop[t-1]
-      sigma.obs <- dnorm(0.056*mu, sqrt(((307.9^2)*35)/s) )
-      obs <- rnorm(1, mu, sigma.obs)
-      h <- ifelse(obs < t_close, rnorm(1, hred, sdh), rnorm(1, hgreen, sdh))
-      h <- ifelse(h < 0, 0, h) #check that h is not negative
-      pop[t] <- pop[t-1] + pop[t-1]*r*(1-(pop[t-1]/K)^theta) - h*pop[t-1]/(1-crip)
-      pop[t] <- ifelse(pop[t]<0,0,pop[t])
-      har[t-1] <- h*pop[t-1]
-    }
-    if(stochastic == TRUE){
-      mu <- q*pop[t-1]
-      sigma.obs <- dnorm(0.056*mu, sqrt(((307.9^2)*35)/s) )
-      obs <- rnorm(1, mu, sigma.obs) 
-      hunt[t-1] <- ifelse(obs < t_close, 0, 1)
-      h <- ifelse(obs < t_close, rnorm(1, hred, sdh), rnorm(1, hgreen, sdh))
-      h <- ifelse(h < 0, 0, h) #check that h is not negative
-      pop[t] <- pop[t-1] + pop[t-1]*r*(1-(pop[t-1]/K)^theta) - h*pop[t-1]/(1-crip)
-      pop[t] <- max(pop[t], 1e-5)
-      pop[t] <- rlnorm(1, log(pop[t]), sdpop)
-      har[t-1] <- h*pop[t-1]  #Note: har[t-1] is the harvest from t-1 to t
-      if(pop[t] < 1) break
-    }
-  }
-  if(total == FALSE) pop <- q*pop
-  return(list(pop = pop, har = har, hunt = hunt))
-}
-
-Nsamples <- 2000
-Tmax <- 100
-pick <- sample(1:length(out$sims.list$r.max), Nsamples)
-results <- matrix(NA, 100, Nsamples)
-for(i in 1:Nsamples){
-  results[,i] <- project.pop(n1 = out$sims.list$N.tot[i,], 
-                             r = out$sims.list$r.max[i], 
-                             theta = out$sims.list$theta[i],
-                             K = out$sims.list$CC[i], 
-                             Hgreen = out$sims.list$mu.green[i],
-                             Hred = out$sims.list$m.har[i],
-                             #sdH = out$sims.list$sigma.har[i],
-                             sdpop = out$sims.list$sigma.proc[i],
-                             q = out$sims.list$q[i],
-                             total = FALSE)$pop
-}
-df <- data.frame(Time = 1:Tmax, results) 
-gplot <- ggplot()
-for(i in 1:100){ #show just 100 samples
-  df2 <- data.frame(Time=df$Time, Pop=df[,i+1])
-  gplot <- gplot + geom_line(data=df2, aes(x=Time, y=Pop))
-}
-gplot <- gplot +
-  labs(x="Year", y="Total Birds") + 
-  geom_hline(yintercept = 23000, color="red")
-print(gplot)
-
-hist(as.vector(results))
-
-#How frequent is season closure
-sum(results < 23000)/(Tmax*Nsamples)
-#How frequency is quasi-extinction
-sum(apply(results,2,min) < 1)/Nsamples
+# project.pop <- function(
+#   #population projection at constant parameters is default
+#   Tmax = 100,
+#   n1 = out$mean$N.tot, #need all population size estimates so we can translate to harvest rate
+#   r = out$mean$r.max, 
+#   theta = out$mean$theta,
+#   K = out$mean$CC, 
+#   Hgreen = out$mean$mu.green,
+#   Hred = out$mean$m.har, 
+#   # sdH = NA,
+#   sdpop = NA,
+#   q = out$mean$q, 
+#   t_close = 23000, #closure threshold, 23000 is 2016 management plan default
+#   total = TRUE, 
+#   stochastic = TRUE){
+#   
+#   pop <- har <- hunt <- numeric() #rep(0, Tmax)
+#   pop[1] <- n1[length(n1)] #starting population size
+#   pop[1] <- n1[37] #starting population size
+#   crip <- rbeta(1, 50, 150) #mean for rbeta =0.25, beta distribution parameters, could sample from posterior
+#   hred <- mean(Hred/n1[1:32])
+#   hgreen <- mean(Hgreen/n1[33:37])
+#   sdh <- sd(Hred/n1[1:32])
+#   s <- rchisq(1, 35) #linear model for observation error
+# 
+#   for(t in 2:(Tmax)){
+#     if(stochastic == FALSE){
+#       mu <- q*pop[t-1]
+#       sigma.obs <- dnorm(0.056*mu, sqrt(((307.9^2)*35)/s) )
+#       obs <- rnorm(1, mu, sigma.obs)
+#       h <- ifelse(obs < t_close, rnorm(1, hred, sdh), rnorm(1, hgreen, sdh))
+#       h <- ifelse(h < 0, 0, h) #check that h is not negative
+#       pop[t] <- pop[t-1] + pop[t-1]*r*(1-(pop[t-1]/K)^theta) - h*pop[t-1]/(1-crip)
+#       pop[t] <- ifelse(pop[t]<0,0,pop[t])
+#       har[t-1] <- h*pop[t-1]
+#     }
+#     if(stochastic == TRUE){
+#       mu <- q*pop[t-1]
+#       sigma.obs <- dnorm(0.056*mu, sqrt(((307.9^2)*35)/s) )
+#       obs <- rnorm(1, mu, sigma.obs) 
+#       hunt[t-1] <- ifelse(obs < t_close, 0, 1)
+#       h <- ifelse(obs < t_close, rnorm(1, hred, sdh), rnorm(1, hgreen, sdh))
+#       h <- ifelse(h < 0, 0, h) #check that h is not negative
+#       pop[t] <- pop[t-1] + pop[t-1]*r*(1-(pop[t-1]/K)^theta) - h*pop[t-1]/(1-crip)
+#       pop[t] <- max(pop[t], 1e-5)
+#       pop[t] <- rlnorm(1, log(pop[t]), sdpop)
+#       har[t-1] <- h*pop[t-1]  #Note: har[t-1] is the harvest from t-1 to t
+#       if(pop[t] < 1) break
+#     }
+#   }
+#   if(total == FALSE) pop <- q*pop
+#   return(list(pop = pop, har = har, hunt = hunt))
+# }
+# 
+# Nsamples <- 2000
+# Tmax <- 100
+# pick <- sample(1:length(out$sims.list$r.max), Nsamples)
+# results <- matrix(NA, 100, Nsamples)
+# for(i in 1:Nsamples){
+#   results[,i] <- project.pop(n1 = out$sims.list$N.tot[i,], 
+#                              r = out$sims.list$r.max[i], 
+#                              theta = out$sims.list$theta[i],
+#                              K = out$sims.list$CC[i], 
+#                              Hgreen = out$sims.list$mu.green[i],
+#                              Hred = out$sims.list$m.har[i],
+#                              #sdH = out$sims.list$sigma.har[i],
+#                              sdpop = out$sims.list$sigma.proc[i],
+#                              q = out$sims.list$q[i],
+#                              total = FALSE)$pop
+# }
+# df <- data.frame(Time = 1:Tmax, results) 
+# gplot <- ggplot()
+# for(i in 1:100){ #show just 100 samples
+#   df2 <- data.frame(Time=df$Time, Pop=df[,i+1])
+#   gplot <- gplot + geom_line(data=df2, aes(x=Time, y=Pop))
+# }
+# gplot <- gplot +
+#   labs(x="Year", y="Total Birds") + 
+#   geom_hline(yintercept = 23000, color="red")
+# print(gplot)
+# 
+# hist(as.vector(results))
+# 
+# #How frequent is season closure
+# sum(results < 23000)/(Tmax*Nsamples)
+# #How frequency is quasi-extinction
+# sum(apply(results,2,min) < 1)/Nsamples
 
 # gplot <- ggplot()
 # for(i in 1:10){
@@ -276,3 +276,89 @@ sum(apply(results,2,min) < 1)/Nsamples
 # 
 # pCloseRed <- sum(t(df2[2,])<23000)/Nsamples
 # pCloseGreen <- sum(t(df[2,])<23000)/Nsamples
+################################################################################
+## modify function for original 2016 data and posterior
+## 
+project.pop2 <- function(
+  #population projection at constant parameters is default
+  Tmax = 100,
+  n1 = out$mean$N.tot, #need all population size estimates so we can translate to harvest rate
+  r = out$mean$r.max, 
+  theta = out$mean$theta,
+  K = out$mean$CC, 
+  Hgreen = out$mean$mu.green,
+  Hred = out$mean$m.har, 
+  # sdH = NA,
+  sdpop = NA,
+  q = out$mean$q, 
+  t_close = 23000, #closure threshold, 23000 is 2016 management plan default
+  total = TRUE, 
+  stochastic = TRUE){
+  
+  crip <- rbeta(1, 50, 150) #mean for rbeta =0.25, beta distribution parameters, could sample from posterior
+  
+  pop <- har <- hunt <- numeric() #rep(0, Tmax)
+  pop[1] <- n1[length(n1)] #starting population size
+  hred <- mean(Hred/n1[1:32])
+  if( length(n1) > 32){ hgreen <- mean(Hgreen/n1[33:length(n1)]) }
+  if( length(n1) == 32) { hgreen <- Hgreen/n1[length(n1)] }
+  sdh <- sd(Hred/n1[1:32]) #should translate to cv so that sd can increase with mean?
+  s <- rchisq(1, 35) #linear model for observation error
+  
+  for(t in 2:(Tmax)){
+    if(stochastic == FALSE){
+      mu <- q*pop[t-1]
+      sigma.obs <- dnorm(0.056*mu, sqrt(((307.9^2)*35)/s) )
+      obs <- rnorm(1, mu, sigma.obs)
+      h <- ifelse(obs < t_close, rnorm(1, hred, sdh), rnorm(1, hgreen, sdh))
+      h <- ifelse(h < 0, 0, h) #check that h is not negative
+      pop[t] <- pop[t-1] + pop[t-1]*r*(1-(pop[t-1]/K)^theta) - h*pop[t-1]/(1-crip)
+      pop[t] <- ifelse(pop[t]<0,0,pop[t])
+      har[t-1] <- h*pop[t-1]
+    }
+    if(stochastic == TRUE){
+      mu <- q*pop[t-1]
+      sigma.obs <- dnorm(0.056*mu, sqrt(((307.9^2)*35)/s) )
+      obs <- rnorm(1, mu, sigma.obs) 
+      hunt[t-1] <- ifelse(obs < t_close, 0, 1)
+      h <- ifelse(obs < t_close, rnorm(1, hred, sdh), rnorm(1, hgreen, sdh))
+      h <- ifelse(h < 0, 0, h) #check that h is not negative
+      pop[t] <- pop[t-1] + pop[t-1]*r*(1-(pop[t-1]/K)^theta) - h*pop[t-1]/(1-crip)
+      pop[t] <- max(pop[t], 1e-5)
+      pop[t] <- rlnorm(1, log(pop[t]), sdpop)
+      har[t-1] <- h*pop[t-1]  #Note: har[t-1] is the harvest from t-1 to t
+      if(pop[t] < 1) break
+    }
+  }
+  if(total == FALSE) pop <- q*pop
+  return(list(pop = pop, har = har, hunt = hunt))
+}
+
+out <- readRDS("out0.RDS")
+Nsamples <- 2000
+Tmax <- 100
+pick <- sample(1:length(out$sims.list$r.max), Nsamples)
+results <- matrix(NA, 100, Nsamples)
+for(i in 1:Nsamples){
+  results[,i] <- project.pop2(n1 = out$sims.list$N.tot[i,], 
+                             r = out$sims.list$r.max[i], 
+                             theta = out$sims.list$theta[i],
+                             K = out$sims.list$CC[i], 
+                             Hgreen = out$sims.list$mu.green[i],
+                             Hred = out$sims.list$m.har[i],
+                             #sdH = out$sims.list$sigma.har[i],
+                             sdpop = out$sims.list$sigma.proc[i],
+                             q = out$sims.list$q[i],
+                             total = FALSE)$pop
+}
+df <- data.frame(Time = 1:Tmax, results) 
+gplot <- ggplot()
+for(i in 1:100){ #show just 100 samples
+  df2 <- data.frame(Time=df$Time, Pop=df[,i+1])
+  gplot <- gplot + geom_line(data=df2, aes(x=Time, y=Pop))
+}
+gplot <- gplot +
+  labs(x="Year", y="Total Birds") + 
+  geom_hline(yintercept = 23000, color="red")
+print(gplot)
+
--- threshold.opt.R
+++ threshold.opt.R
@@ -4,7 +4,6 @@ out <- readRDS("out2.RDS")
 #define threshold to search across; this is observed YKD index values to search over
 t_red <- seq(0, 50000, by= 250)
 Nsamples <- 10000 #number of samples of the posterior
-pick <- sample(1:length(out$sims.list$r.max), Nsamples)
 TimeHor <- 200 #time over which to calculate return/yield; time horizon
 discount <- 1 #time discount rate
 df <- data.frame(Closure=t_red,
@@ -14,9 +13,10 @@ df <- data.frame(Closure=t_red,
 
 #set up loops
 temp <- list(cumhar = numeric(), phunt = numeric(), mPop = numeric())
+pick <- sample(1:length(out$sims.list$r.max), Nsamples)
 for(t in 1:length(t_red)){
   for(i in 1:Nsamples){
-      reward <- project.pop(Tmax = TimeHor+1,
+      reward <- project.pop2(Tmax = TimeHor+1,
                             n1 = out$sims.list$N.tot[pick[i],], 
                             r = out$sims.list$r.max[pick[i]], 
                             theta = out$sims.list$theta[pick[i]],
@@ -37,6 +37,7 @@ for(t in 1:length(t_red)){
 }
 
 saveRDS(df, file = "optim.RDS")
+df <- readRDS("optim.RDS")
 ggplot(data = df, aes(x=Closure, y=cumHar)) + 
   geom_point()+
   geom_smooth(method="gam", se=TRUE)
@@ -46,8 +47,69 @@ ggplot(data = df, aes(x=Closure, y=pHunt)) +
 ggplot(data = df, aes(x=Closure, y=mPop)) + 
   geom_point()+
   geom_smooth(method="gam", se=TRUE)
+#yield curve
+ggplot(data = df, aes(x=mPop, y=cumHar)) + 
+  geom_point()+
+  geom_smooth(method="gam", se=TRUE)
 #find MSY closure level
 library(mgcv)
 fit <- gam(cumHar~s(Closure), data=df)
 pred <- predict(fit)
 df[which(pred == max(pred)),]
+#find % yield curve with min harvest
+slice_tail(df)$cumHar/df[which(pred == max(pred)),]$cumHar
+#97%!
+################################################################################
+## Now for the 2016 posterior
+#load data
+out <- readRDS("out0.RDS")
+#define threshold to search across; this is observed YKD index values to search over
+t_red <- seq(0, 50000, by= 250)
+Nsamples <- 10000 #number of samples of the posterior
+TimeHor <- 200 #time over which to calculate return/yield; time horizon
+discount <- 1 #time discount rate
+df <- data.frame(Closure=t_red,
+                 cumHar=rep(NA, length(t_red)),
+                 pHunt=rep(NA, length(t_red)),
+                 mPop = rep(NA, length(t_red)))
+
+#set up loops
+temp <- list(cumhar = numeric(), phunt = numeric(), mPop = numeric())
+pick <- sample(1:length(out$sims.list$r.max), Nsamples)
+for(t in 1:length(t_red)){
+  for(i in 1:Nsamples){
+    reward <- project.pop2(Tmax = TimeHor+1,
+                           n1 = out$sims.list$N.tot[pick[i],], 
+                           r = out$sims.list$r.max[pick[i]], 
+                           theta = out$sims.list$theta[pick[i]],
+                           K = out$sims.list$CC[pick[i]], 
+                           Hgreen = out$sims.list$mu.green[pick[i]],
+                           Hred = out$sims.list$m.har[pick[i]],
+                           sdpop = out$sims.list$sigma.proc[pick[i]],
+                           q = out$sims.list$q[pick[i]],
+                           t_close = t_red[t],
+                           total = TRUE)
+    temp$cumhar[i] <- sum(reward$har, na.rm = TRUE)
+    temp$phunt[i] <- sum(reward$hunt, na.rm = TRUE)/length(reward$hunt[!is.na(reward$hunt)])
+    temp$mPop[i] <- mean(reward$pop, na.rm = TRUE)
+  }
+  df$cumHar[t] <- mean(temp$cumhar)
+  df$pHunt[t] <- mean(temp$phunt)
+  df$mPop[t] <- mean(temp$mPop)
+}
+
+saveRDS(df, file = "optim0.RDS")
+df <- readRDS("optim.RDS")
+ggplot(data = df, aes(x=Closure, y=cumHar)) + 
+  geom_point()+
+  geom_smooth(method="gam", se=TRUE)
+ggplot(data = df, aes(x=Closure, y=pHunt)) + 
+  geom_point()+
+  geom_smooth(method="gam", se=TRUE)
+ggplot(data = df, aes(x=Closure, y=mPop)) + 
+  geom_point()+
+  geom_smooth(method="gam", se=TRUE)
+#yield curve
+ggplot(data = df, aes(x=mPop, y=cumHar)) + 
+  geom_point()+
+  geom_smooth(method="gam", se=TRUE)
